2ac5a99204ee9067b584f6a3c53c6547124212bc
Tayphoon <>
2019-01-11 10:47:56 +0300

    Nullability

--- File Templates/TCCollectionCellObject.xctemplate/___FILEBASENAME___CellObject.h
+++ File Templates/TCCollectionCellObject.xctemplate/___FILEBASENAME___CellObject.h
@@ -10,8 +10,12 @@
 
 #import "TCCollectionCellObject.h"
 
+NS_ASSUME_NONNULL_BEGIN
+
 @interface ___VARIABLE_cellClass:identifier___CellObject : NSObject<TCCollectionCellObject>
 
 @property (nonatomic, strong) id item;
 
 @end
+
+NS_ASSUME_NONNULL_END
